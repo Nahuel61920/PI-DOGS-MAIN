d39ef5b38933e315db9aa152aa2fc167024eec6c
Nahuel61920 <estebancarrizo619@gmail.com>
2022-07-19 00:40:37 -0300

    End DB

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,40 +1,53 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("Dog", {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      primaryKey: true,
+  sequelize.define(
+    "dog",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      weightMin: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      weightMax: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      heightMin: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      heightMax: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lifeMax: {
+        type: DataTypes.STRING,
+      },
+      lifeMin: {
+        type: DataTypes.STRING,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdInBd: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    height: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    weight: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    image: {
-      type: DataTypes.STRING,
-    },
-    life_span: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    createdInDb: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true,
-    },
-  },
-  
-  {timestamps: false}
+    { timestamps: false }
   );
-};
+}
---
+++ api/src/models/Temp.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define(
+    "temp",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: true
+    }
+    }
+  );
+};
