f8643cc884157ada74807211cadf1faaff8a499b
Nahuel61920 <estebancarrizo619@gmail.com>
2022-08-07 11:53:11 -0300

    Responsive 80%

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -10,39 +10,60 @@ import SearchSvg2 from '../SVG/searchSvg2';
 function Nav({setCurrentPage}) {
     const [ open , setOpen ] = useState(false);
 
-  return (
-    <div className={styles.nav_container}>
-        <nav className={styles.nav}>
-            <div className={styles.navbar}>
-                <div className={styles.logo_container}>
-                    <Link to={'/'}>
-                        <img src={logo} alt="logo" className={styles.logo} />
-                    </Link>
-                </div>
-                <div className={styles.create}>
-                    <Link to={'/wallpaper'} style={{ textDecoration: 'none' }}>
-                        <p>Wallpapers</p>
-                    </Link>
-                    <Link to={'/create-dog'}>
-                        <button className={styles.create_button}>Create Dog</button>
-                    </Link>
-                    <p>About</p>
-                </div>
-                <div className={styles.search}>
-                    <DarkMode/>
-                </div>
-                <div className={styles.lupa} onClick={ () => setOpen(!open)}>
-                    <SearchSvg2/>
-                </div>
+    const [clicked, setClicked] = useState(false)
+
+    const handleClick = () => {
+        //cuando esta true lo pasa a false y vice versa
+        setClicked(!clicked)
+    }
+
+    return (
+        <div className={styles.nav_container}>
+            <nav className={styles.nav}>
+                <div className={styles.navbar}>
+                    <div className={`icon nav-icon-1 ${clicked? "open" : ""}`} onClick={handleClick}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <div className={styles.logo_container}>
+                        <Link to={'/'}>
+                            <img src={logo} alt="logo" className={styles.logo} />
+                        </Link>
+                    </div>
+                    <div className={`create ${clicked ? 'active' : ''}`}>
+                        <Link to={'/wallpaper'} style={{ textDecoration: 'none' }}>
+                            <p>Wallpapers</p>
+                        </Link>
+                        <Link to={'/create-dog'}>
+                            <button className={styles.create_button}>Create Dog</button>
+                        </Link>
+                        <p>About</p>
+                    </div>
+                    <div className={styles.search}>
+                        <DarkMode/>
+                    </div>
+                    <div className={styles.lupa} onClick={ () => setOpen(!open)}>
+                        <SearchSvg2/>
+                    </div>
 
 
-            </div>
-            <div className={ open ? "searchBarOpen" : "serchBar" }>
-                <SearchBar setCurrentPage={setCurrentPage}/>
-            </div>
-        </nav>
-    </div>
-  )
+                </div>
+                <div className={ open ? "searchBarOpen" : "serchBar" }>
+                    <SearchBar setCurrentPage={setCurrentPage}/>
+                </div>
+                <div className={`initial ${clicked ? ' active' : ''}`}>
+                    <h1>Wallpapers</h1>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default Nav
